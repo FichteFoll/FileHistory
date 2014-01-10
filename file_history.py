9f528a98d7e14302bb10ac401baa2c0dea4302da
Josh Bjornson <jbjornson@gmail.com>
2014-01-10 11:10:09 +0100

    Removed creative commons license (MIT license file added to project)

--- file_history.py
+++ file_history.py
@@ -1,11 +1,3 @@
-'''
-@author Josh Bjornson
-
-This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
-To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/
-or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
-'''
-
 # TODO some restructuring
 # DONE option to cleanup the history database (json) on start
 # DONE introduce a settings file to get settings from
